bd819e3049dd90f494aa6761176f30d80dbd1ff6
AmarArjunGunjal <amargunjal28@gmail.com>
2020-01-29 15:44:43 +0530

    Added : Convert and check addition of litres and mililitres equals litres

--- src/test/java/quantity_measurement/OperationsTest.java
+++ src/test/java/quantity_measurement/OperationsTest.java
@@ -59,4 +59,14 @@ public class OperationsTest {
         double litreResult = lengthMeasurement.getConversion(result, QuantityMeasurement.Units.LITRE);
         Assert.assertEquals(7.57,litreResult,0);
     }
+
+    @Test
+    public void whenGivenLitreAndMililitre_shouldReturnLitres() {
+        QuantityMeasurement lengthMeasurement = new QuantityMeasurement();
+        double value = UnitType.LITRES.getInchValue(1.0);
+        double value1 = UnitType.ML.getInchValue(1000.0);
+        double result= lengthMeasurement.getAddition(value, value1);
+        double litreResult = lengthMeasurement.getConversion(result, QuantityMeasurement.Units.LITRE);
+        Assert.assertEquals(2.0,litreResult,0);
+    }
 }
